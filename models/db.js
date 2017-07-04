ad374121f9e3f45ff771635ee51b53b520dd295c
Andrew Aaronson <aaaronson@Andrews-MacBook-Pro.local>
2017-07-04 19:07:25 -0400

    typo

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const sequelize = require('sequelize');
 const pg = require('pg');
 
 pg.defaults.ssl = true;
-pg.connect(proces.env.DATABASE_URL, (err, client) => {
+pg.connect(process.env.DATABASE_URL, (err, client) => {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
 
