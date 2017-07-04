68b42e00c19883724496c64b146d248a3f79b978
Andrew Aaronson <aaaronson@Andrews-MacBook-Pro.local>
2017-07-04 19:28:00 -0400

    changing models

--- models/link.js
+++ models/link.js
@@ -1,28 +1,52 @@
-const db = require('./db');
-const sequelize = require('sequelize');
+// const db = require('./db');
+// const sequelize = require('sequelize');
 
-const categories = ['Apps', 'Boondocking', 'Cold Weather', 'Mail', 'Maintenance', 'Internet', 'Renovating & Decorating'];
+// const categories = ['Apps', 'Boondocking', 'Cold Weather', 'Mail', 'Maintenance', 'Internet', 'Renovating & Decorating'];
 
-const Link = db.define('link', {
+// const Link = db.define('link', {
+//   title: {
+//     type: sequelize.STRING,
+//     allowNull: false
+//   },
+//   author: {
+//     type: sequelize.STRING,
+//     allowNull: false
+//   },
+//   url: {
+//     type: sequelize.STRING,
+//     allowNull: false
+//   },
+//   category: {
+//     type: sequelize.STRING,
+//     allowNull: false
+//   },
+//   types: {
+//     type: sequelize.ARRAY(sequelize.STRING)
+//   }
+// });
+
+// module.exports = Link;
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('link', {
   title: {
-    type: sequelize.STRING,
+    type: DataTypes.STRING,
     allowNull: false
   },
   author: {
-    type: sequelize.STRING,
+    type: DataTypes.STRING,
     allowNull: false
   },
   url: {
-    type: sequelize.STRING,
+    type: DataTypes.STRING,
     allowNull: false
   },
   category: {
-    type: sequelize.STRING,
+    type: DataTypes.STRING,
     allowNull: false
   },
   types: {
-    type: sequelize.ARRAY(sequelize.STRING)
+    type: DataTypes.ARRAY(DataTypes.STRING)
   }
-});
-
-module.exports = Link;
+})
+}
