8973075cf36094e3c2b89a08fee4ae5ac8c36e33
Andrew Aaronson <aaaronson@Andrews-MacBook-Pro.local>
2017-07-04 19:04:57 -0400

    troubleshooting

--- models/db.js
+++ models/db.js
@@ -1,3 +1,16 @@
 const sequelize = require('sequelize');
+const pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(proces.env.DATABASE_URL, (err, client) => {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', row => {
+      console.log(JSON.stringify(row));
+    });
+});
 
 module.exports = new sequelize('postgres://localhost:5432/rvsource', { logging: false });
